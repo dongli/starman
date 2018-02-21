cacf3f12928fcbb22f5b2b15168918ac5ed4c2f3
Li Dong <dongli@lasg.iap.ac.cn>
2018-02-22 00:04:16 +0800

    Add jpeg v9b

---
+++ packages/jpeg.rb
@@ -0,0 +1,15 @@
+class Jpeg < Package
+  url 'http://www.ijg.org/files/jpegsrc.v9b.tar.gz'
+  sha256 '240fd398da741669bf3c90366f58452ea59041cacc741a489b99f2f6a0bad052'
+  version 'v9b'
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+    run './configure', *args
+    run 'make', 'install'
+  end
+end
