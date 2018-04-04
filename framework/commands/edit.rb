d72c47e81a5ffe195ff18a437042bb4c0e2ace9b
Li Dong <dongli@lasg.iap.ac.cn>
2018-04-04 10:41:56 +0800

    Add edit command

---
+++ framework/commands/edit.rb
@@ -0,0 +1,17 @@
+class EditCommand < CommandParser
+  include Utils
+
+  def initialize
+    super
+    @parser.banner += <<-EOS
+
+    >>> starman edit <package_name>
+EOS
+    @parser.parse!
+    Settings.init
+  end
+
+  def run
+    system "vim -c 'set filetype=ruby' #{ENV['STARMAN_ROOT']}/packages/#{ARGV.last}.rb"
+  end
+end
