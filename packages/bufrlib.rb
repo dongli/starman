a9af5b57444efc7511d498e17cdf0d9b655949f9
Li Dong <dongli@lasg.iap.ac.cn>
2018-09-06 14:18:53 +0800

    Fix bufrlib on macOS

--- packages/bufrlib.rb
+++ packages/bufrlib.rb
@@ -12,16 +12,26 @@ class Bufrlib < Package
       }
     elsif CompilerSet.fortran.intel?
       flags = '-assume nounderscore'
+      inreplace 'preproc.sh', 'cpp', 'fpp'
     else
       flags = ''
     end
-    inreplace 'preproc.sh', 'cpp', 'fpp' if CompilerSet.fortran.intel?
-    run '$CC -c `./preproc.sh` *.c'
-    run '$FC ', flags, ' -c modv*.f moda*.f `ls -1 *.f | grep -v "mod[av]_"`'
-    run 'ar crv libbufr.a *.o'
+    # Since macOS isn't case-sensitive, we need to build in a new directory!
+    inreplace 'preproc.sh', {
+      '`ls *.F`' => '`ls ../*.F`',
+      'bufrlib.PRM' => '../bufrlib.PRM'
+    }
+    mkdir 'build' do
+      ln '../*.h', '.'
+      ln '../*.c', '.'
+      ln '../*.f', '.'
+      run '$CC -c `../preproc.sh` *.c'
+      run '$FC ', flags, ' -c modv*.f moda*.f `ls -1 *.f | grep -v "mod[av]_"`'
+      run 'ar crv libbufr.a *.o'
+    end
     mkdir inc
     mkdir lib
     mv '*.h', inc
-    mv 'libbufr.a', lib
+    mv './build/libbufr.a', lib
   end
 end
