2c6bdfe5e4786716f65e55130915cc676ee0889c
Li Dong <dongli@lasg.iap.ac.cn>
2018-09-15 11:34:00 +0800

    Change openmpi to use ucx instead of mxm

--- packages/openmpi.rb
+++ packages/openmpi.rb
@@ -1,6 +1,6 @@
 class Openmpi < Package
-  url 'https://www.open-mpi.org/software/ompi/v3.0/downloads/openmpi-3.0.0.tar.bz2'
-  sha256 'f699bff21db0125d8cccfe79518b77641cd83628725a1e1ed3e45633496a82d7'
+  url 'https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.bz2'
+  sha256 'c654ed847f34a278c52a15c98add40402b4a90f0c540779f1ae6c489af8a76c5'
 
   label :mpi
 
@@ -15,12 +15,8 @@ class Openmpi < Package
       --enable-shared
       --with-verbs
       --with-hwloc=internal
-      --without-ucx
+      --with-ucx
     ]
-    # FIXME: Check if we need to add MXM and KNEM explicitly, or do they affect performance.
-    args << '--with-mxm=/opt/mellanox/mxm' if File.directory? '/opt/mellanox/mxm'
-    dir = `find /opt -maxdepth 1 -type d -name "knem*" -print0`.strip
-    args << "--with-knem=#{dir}" if dir and File.directory? dir
     run './configure', *args
     run 'make', 'all', '-j', '8'
     run 'make', 'check' if not skip_test?
