353abe5f8223f8955378ab1aabe50642c2774bf4
Li Dong <dongli@lasg.iap.ac.cn>
2019-05-06 20:28:53 +0800

    Update gcc to 9.1.0

--- packages/gcc.rb
+++ packages/gcc.rb
@@ -1,6 +1,6 @@
 class Gcc < Package
-  url 'http://mirrors.ustc.edu.cn/gnu/gcc/gcc-8.2.0/gcc-8.2.0.tar.xz'
-  sha256 '196c3c04ba2613f893283977e6011b2345d1cd1af9abeac58e916b1aab3e0080'
+  url 'http://mirrors.ustc.edu.cn/gnu/gcc/gcc-9.1.0/gcc-9.1.0.tar.xz'
+  sha256 '79a66834e96a6050d8fe78db2c3b32fb285b230b855d0a66288235bc04b327a0'
 
   label :compiler
 
