f3c0a7f6713edb5ab81f422716a994f4c31e99fb
Li Dong <dongli@lasg.iap.ac.cn>
2019-05-08 17:49:12 +0800

    Fix gcc build on RHEL

--- packages/gcc.rb
+++ packages/gcc.rb
@@ -30,6 +30,16 @@ class Gcc < Package
     append_ld_library_path "#{lib}/gcc/lib64"
   end
 
+  def build_name
+    if OS.mac?
+      'x86_64-apple-darwin'
+    elsif OS.linux?
+      'x86_64-pc-linux-gnu'
+    else
+      'unknown'
+    end
+  end
+
   def install
     ENV['LIBRARY_PATH'] = ''
     if CompilerSet.c.version <= '4.8.5'
@@ -37,8 +47,15 @@ class Gcc < Package
       inreplace 'gcc/Makefile.in', 'mv tmp-specs $(SPECS)', '#mv tmp-specs $(SPECS)'
       self.disable_lto = true
     end
+    if OS.redhat?
+      CLI.warning 'STARMAN is trying hard to fix GCC build for Red Hat Enterprise Linux!'
+      Dir.glob('libgfortran/generated/*.F90').concat(['libgfortran/libgfortran.h', 'libgfortran/ieee/ieee_exceptions.F90', 'libgfortran/ieee/ieee_arithmetic.F90']).each do |file|
+        inreplace file, '"config.h"', "\"#{pwd}/../gcc-build/#{build_name}/libgfortran/config.h\""
+      end
+    end
     args = %W[
       --prefix=#{prefix}
+      --build=#{build_name}
       --libdir=#{lib}/gcc/#{version.to_s.slice(/\d/)}
       --enable-languages=c,c++,fortran
       --disable-multilib
@@ -60,9 +77,9 @@ class Gcc < Package
     mv 'isl-0.18', 'isl'
     mkdir '../gcc-build' do
       run "../gcc-#{version}/configure", *args
-      run 'make', '-j4', 'bootstrap'
+      run 'make', '-j8', 'bootstrap'
       #run 'ulimit -s 32768 && make -k check' unless skip_test?
-      run 'make', '-j4', 'install'
+      run 'make', '-j8', 'install'
     end
   end
 
