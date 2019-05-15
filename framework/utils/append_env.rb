28ceede79399785627adb1612bd0590d31121991
Li Dong <dongli@lasg.iap.ac.cn>
2019-05-15 20:26:09 +0800

    append_env should set ENV

--- framework/utils/append_env.rb
+++ framework/utils/append_env.rb
@@ -20,6 +20,7 @@ module Utils
   def append_env key, val
     @@appended_env[key] = ENV[key] if not @@appended_env.has_key? key
     @@appended_env[key] = "#{val}:#{@@appended_env[key].gsub(val, '') if @@appended_env[key]}".gsub('::', ':')
+    ENV[key] = @@appended_env[key]
   end
 
   def appended_env
