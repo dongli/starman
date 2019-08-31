5cdd20290e4bc42ee4dc692b396ea252814b31b4
Li Dong <dongli@lasg.iap.ac.cn>
2019-08-31 15:34:08 +0800

    Update met to 8.1.1

--- packages/met.rb
+++ packages/met.rb
@@ -1,7 +1,7 @@
 class Met < Package
-  url 'https://dtcenter.org/met/users/downloads/MET_releases/met-8.0.20180927.tar.gz'
-  sha256 '3d1674d7d6f63590caed9dc8a16e51e36ab7e0cefaa0abee0626013462970ba2'
-  version '8.0'
+  url 'https://dtcenter.org/sites/default/files/community-code/met/met-8.1.1.20190708.tar.gz'
+  sha256 'b5b7e0f3c1d2b786f9ea625bce1fcd67d578dd1e73bd97825200e35428ecc745'
+  version '8.1.1'
 
   label :common
 
