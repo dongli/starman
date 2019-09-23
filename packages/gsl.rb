1eb9ad08750398f5cb1062f862a378e2a8e4adf6
Li Dong <dongli@lasg.iap.ac.cn>
2019-09-24 07:34:14 +0800

    Update gsl to 2.6

--- packages/gsl.rb
+++ packages/gsl.rb
@@ -1,7 +1,7 @@
 class Gsl < Package
-  url 'https://ftp.gnu.org/gnu/gsl/gsl-2.5.tar.gz'
-  mirror 'https://ftpmirror.gnu.org/gsl/gsl-2.5.tar.gz'
-  sha256 '0460ad7c2542caaddc6729762952d345374784100223995eb14d614861f2258d'
+  url 'https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz'
+  mirror 'https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz'
+  sha256 'b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8'
 
   def install
     run './configure', '--disable-dependency-tracking', "--prefix=#{prefix}"
