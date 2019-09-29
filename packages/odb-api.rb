ced49c6f6c5f4521a558368624bac14d24f8be6c
Li Dong <dongli@lasg.iap.ac.cn>
2019-09-29 16:09:39 +0800

    Fix odb-api with ncurses

--- packages/odb-api.rb
+++ packages/odb-api.rb
@@ -2,6 +2,7 @@ class OdbApi < Package
   url 'https://confluence.ecmwf.int/download/attachments/61117379/odb_api_bundle-0.18.1-Source.tar.gz?api=v2'
   sha256 '7a01968be0d6f55004d6203a4b9ee06b91accec9078da4103cf71d2879ca2aad'
 
+  depends_on :cmake
   depends_on :yacc
   depends_on :ncurses
   depends_on :eccodes
@@ -15,9 +16,15 @@ class OdbApi < Package
     args << '-DENABLE_FORTRAN=On'
     args << "-DENABLE_PYTHON=#{with_python? ? 'On' : 'Off'}"
     args << "-DNETCDF_PATH='#{link_inc}'"
+    args << "-DCURSES_INCLUDE_PATH=#{Ncurses.inc}"
+    args << "-DCURSES_LIBRARY=#{Ncurses.lib}/libncursesw.#{OS.soname}"
     if OS.mac?
       inreplace 'odb_api/CMakeLists.txt', 'ecbuild_add_cxx_flags("-fPIC -Wl,--as-needed")', 'ecbuild_add_cxx_flags("-fPIC")'
     end
+    inreplace 'eckit/src/eckit/cmd/term.c', {
+      '<curses.h>' => '<ncursesw/curses.h>',
+      '<term.h>' => '<ncursesw/term.h>'
+    }
     mkdir 'build' do
       run 'cmake', '..', *args
       run 'make', 'install'
