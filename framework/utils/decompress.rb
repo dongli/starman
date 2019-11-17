23257025d94131de235cb72846bbd1e7765e45b9
Li Dong <dongli@lasg.iap.ac.cn>
2019-11-17 12:09:09 +0000

    Fix install_ruby zlib depends and add install_sqlite

--- framework/utils/decompress.rb
+++ framework/utils/decompress.rb
@@ -13,12 +13,13 @@ module Utils
     when /\.(bz2)$/i
       system "bzip2 -d #{path}"
     when /\.(zip)$/i
+      CLI.error 'No unzip is installed by system! Please install one by apt-get or yum according to your OS.' if not system_command? 'unzip'
       system "unzip -o #{path} 1> /dev/null"
       if options[:strip_leading_dirs]
         system 'mv ./*/* .'
       end
     else
-      CLI.report_error "Unknown compression type of #{CLI.red path}!" if options[:exit]
+      CLI.error "Unknown compression type of #{CLI.red path}!" if options[:exit]
     end
   end
 end
