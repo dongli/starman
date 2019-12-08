de2d491d10270f9c91eca449ec5a23a4d2857445
Li Dong <dongli@lasg.iap.ac.cn>
2019-12-08 08:34:53 +0800

    Test build openarray 1.0.1

--- packages/openarray.rb
+++ packages/openarray.rb
@@ -1,7 +1,7 @@
 class Openarray < Package
   url 'https://github.com/hxmhuang/OpenArray/archive/1.0.1.tar.gz'
-  sha256 ''
-  version '1.0.1'
+  sha256 '3a21246ab9ca2e569304437fe05f504bb2778fbd1ed8a43a61692a0c0be6150e'
+  file_name 'openarray-1.0.1.tar.gz'
 
   depends_on :pnetcdf
   depends_on :mpi
@@ -13,6 +13,7 @@ class Openarray < Package
     ]
     run './configure', *args
     run 'make'
+    run 'make', 'test' unless skip_test?
     run 'make', 'install'
   end
 end
