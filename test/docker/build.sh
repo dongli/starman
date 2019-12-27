804ea7a77b2e0a97645cf18eb8be0a483c7eb89f
Li Dong <dongli@lasg.iap.ac.cn>
2019-12-27 12:36:21 +0800

    Update openmpi to 4.0.2

--- test/docker/build.sh
+++ test/docker/build.sh
@@ -2,7 +2,7 @@
 
 VERSION=0.0.1
 
-sed -i '' "s/version \".*\"/version \"$VERSION\"/" Dockerfile
+sed -i "s/version \".*\"/version \"$VERSION\"/" Dockerfile
 
 docker build --rm -t dongli/starman-test:$VERSION . && \
 docker push dongli/starman-test:$VERSION
