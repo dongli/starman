52773dd68524e44c70af3eeaa821e2d34f17a227
Li Dong <dongli@lasg.iap.ac.cn>
2021-02-15 00:47:31 +0800

    Change udunits url

--- packages/udunits.rb
+++ packages/udunits.rb
@@ -1,7 +1,6 @@
 class Udunits < Package
-  url 'https://artifacts.unidata.ucar.edu/repository/downloads-udunits/udunits-2.2.28-Source.tar.gz'
-  sha256 '4cff332db4368c621998116603ad2d35cfd4a605e60a77e9a7270aed7d905711'
-  version '2.2.28'
+  url 'https://artifacts.unidata.ucar.edu/repository/downloads-udunits/udunits-2.2.28.tar.gz'
+  sha256 '590baec83161a3fd62c00efa66f6113cec8a7c461e3f61a5182167e0cc5d579e'
 
   label :common
 
