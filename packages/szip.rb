bcb3969ccc61f11c0137aaf88d648b4d2d09abcd
Li Dong <dongli@lasg.iap.ac.cn>
2021-11-16 16:16:57 +0800

    Fix build szip using icc

--- packages/szip.rb
+++ packages/szip.rb
@@ -3,6 +3,7 @@ class Szip < Package
   sha256 '21ee958b4f2d4be2c9cabfa5e1a94877043609ce86fde5f286f105f7ff84d412'
 
   def install
+    ENV['CFLAGS'] = '-fPIC' if CompilerSet.c.intel?
     args = %W[
       --prefix=#{prefix}
       --disable-debug
