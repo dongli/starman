0fd807b713a7386d4c68fc5da6571b7ce1b6a43b
Li Dong <dongli@lasg.iap.ac.cn>
2022-08-06 03:51:48 +0000

    Forget add sha256 in openmpi@4.0.1

--- packages/openmpi@4.0.1.rb
+++ packages/openmpi@4.0.1.rb
@@ -1,6 +1,6 @@
 class Openmpi < Package
   url 'https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.1.tar.bz2'
-  sha256 ''
+  sha256 'cce7b6d20522849301727f81282201d609553103ac0b09162cf28d102efb9709'
 
   label :mpi
 
