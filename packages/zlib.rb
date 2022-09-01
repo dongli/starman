04b5e500222a3eccf0fc1edca46e1923579678e0
Li Dong <dongli@lasg.iap.ac.cn>
2022-09-01 15:00:27 +0800

    Test use system zlib

--- packages/zlib.rb
+++ packages/zlib.rb
@@ -2,7 +2,7 @@ class Zlib < Package
   url 'https://www.zlib.net/zlib-1.2.12.tar.gz'
   sha256 '91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9'
 
-  label :conflict_with_system
+  label :skip_if_exist, library_file: 'libz.so'
 
   def install
     ENV['CFLAGS'] = '-O3 -fPIC'
