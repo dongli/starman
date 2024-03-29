0c0d62946c960bcd75d046835fbf0f656e163aa0
Li Dong <dongli@lasg.iap.ac.cn>
2024-03-29 08:42:13 +0800

    Update gsl to 2.7

--- packages/gsl.rb
+++ packages/gsl.rb
@@ -1,11 +1,10 @@
 class Gsl < Package
-  url 'https://ftp.gnu.org/gnu/gsl/gsl-2.6.tar.gz'
-  mirror 'https://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz'
-  sha256 'b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8'
+  url 'https://mirrors.sjtug.sjtu.edu.cn/gnu/gsl/gsl-2.7.tar.gz'
+  sha256 ''
 
   def install
     run './configure', '--disable-dependency-tracking', "--prefix=#{prefix}"
-    run 'make'
+    run 'make', multiple_jobs? ? "-j#{jobs_number}" : ''
     run 'make', 'install'
   end
 end
