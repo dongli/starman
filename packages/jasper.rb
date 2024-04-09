54c10f1edb7418e2937cfe96763e8110af168a43
Li Dong <dongli@lasg.iap.ac.cn>
2024-04-09 04:43:12 +0000

    Fix jasper for newer icx compiler

--- packages/jasper.rb
+++ packages/jasper.rb
@@ -5,11 +5,19 @@ class Jasper < Package
   depends_on :jpeg
 
   def install
+    inreplace 'src/libjasper/jpc/jpc_qmfb.c', {
+      'int jpc_ft_synthesize(int *a' => 'int jpc_ft_synthesize(jpc_fix_t *a'
+    }
+    inreplace 'src/libjasper/jpc/jpc_qmfb.h', {
+      'int (*analyze)(int *' => 'int (*analyze)(long *',
+      'int (*synthesize)(int *' => 'int (*synthesize)(long *'
+    }
     args = %W[
       --disable-debug
       --disable-dependency-tracking
       --enable-shared
       --prefix=#{prefix}
+      CFLAGS='-std=c90'
       CPPFLAGS='-I#{Jpeg.inc}'
       LDFLAGS='-L#{Jpeg.lib}'
     ]
