03dd058ff8074d9b2fba6e3e6bfb618532ba79f0
Li Dong <dongli@lasg.iap.ac.cn>
2024-05-15 07:09:11 +0800

    Update munge to 0.5.16

--- packages/munge.rb
+++ packages/munge.rb
@@ -8,8 +8,8 @@
 # platform-specific methods.
 
 class Munge < Package
-  url 'https://github.com/dun/munge/archive/munge-0.5.14.tar.gz'
-  sha256 '5d97b4df164f26e0fb9b86a215e646b2311fa79a2fe9aaaeac067f4ed906220a'
+  url 'https://github.com/dun/munge/archive/refs/tags/munge-0.5.16.tar.gz'
+  sha256 'fa27205d6d29ce015b0d967df8f3421067d7058878e75d0d5ec3d91f4d32bb57'
 
   label :common
   label :skip_if_exist, binary_file: 'munge'
@@ -25,7 +25,7 @@ class Munge < Package
     run './bootstrap'
     run './configure', *args
     run 'make'
-    #run 'make', 'check' if not skip_test?
+    run 'make', 'check' if not skip_test?
     run 'make', 'install'
     run "chmod 0600 #{prefix}/etc/munge/munge.key"
   end
