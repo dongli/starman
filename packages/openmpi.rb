aebea8aaed536bb23a923549267f893367a2cd73
Li Dong <dongli@lasg.iap.ac.cn>
2024-05-27 13:01:17 +0800

    Update openmpi to 5.0.3 and revise depends

--- packages/openmpi.rb
+++ packages/openmpi.rb
@@ -1,15 +1,20 @@
 class Openmpi < Package
-  url 'https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.0.tar.bz2'
-  sha256 '9d845ca94bc1aeb445f83d98d238cd08f6ec7ad0f73b0f79ec1668dbfdacd613'
+  url 'https://download.open-mpi.org/release/open-mpi/v5.0/openmpi-5.0.3.tar.bz2'
+  sha256 '990582f206b3ab32e938aa31bbf07c639368e4405dca196fabe7f0f76eeda90b'
 
   label :mpi
 
   conflicts_with :mpich, :mvapich2
 
-  option 'with-ucx', 'Use UCX library.'
-  option 'with-verbs', 'Use VERBS library.'
+  option 'with-external-libs', 'Use STARMAN built libraries (libevent, hwloc, pmix, ucx).'
 
   depends_on :zlib
+  if with_external_libs?
+    depends_on :hwloc
+    depends_on :pmix
+    depends_on :libevent
+    depends_on :ucx
+  end
 
   def export_env
     ENV['MPICC'] = "#{bin}/mpicc"
@@ -25,17 +30,18 @@ class Openmpi < Package
       --disable-debug
       --disable-sphinx
       --enable-shared
-      --with-libevent=internal
-      --with-pmix=internal
-      --with-prrte=internal
-      --with-hwloc=internal
       --with-zlib=#{Zlib.prefix}
+      --with-prrte=internal
     ]
-    args << '--with-ucx' if with_ucx?
-    args << '--with-verbs' if with_verbs?
+    if with_external_libs?
+      args << "--with-libevent=#{Libevent.prefix}"
+      args << "--with-hwloc=#{Hwloc.prefix}"
+      args << "--with-pmix=#{Pmix.prefix}"
+      args << "--with-ucx=#{Ucx.prefix}"
+    end
     ENV['lt_cv_ld_force_load'] = 'no' if OS.mac?
     run './configure', *args
-    run 'make', 'all', '-j', '8'
+    run 'make', 'all', multiple_jobs? ? "-j#{jobs_number}" : ''
     run 'make', 'check' if not skip_test?
     run 'make', 'install'
   end
