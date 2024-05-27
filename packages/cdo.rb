625d07c254c31e95e9ed390cdf8e0b52cfe69938
Li Dong <dongli@lasg.iap.ac.cn>
2024-05-27 13:00:51 +0800

    Update cdo to 2.3.0

--- packages/cdo.rb
+++ packages/cdo.rb
@@ -1,16 +1,14 @@
 class Cdo < Package
-  url 'https://code.mpimet.mpg.de/attachments/download/28882/cdo-2.2.2.tar.gz'
-  sha256 '419c77315244019af41a296c05066f474cccbf94debfaae9e2106da51bc7c937'
+  url 'https://code.mpimet.mpg.de/attachments/download/29019/cdo-2.3.0.tar.gz'
+  sha256 '10c878227baf718a6917837527d4426c2d0022cfac4457c65155b9c57f091f6b'
 
   label :common
 
   depends_on :eccodes
   depends_on :hdf5
-  depends_on :libxml2
+  depends_on :libaec
   depends_on :netcdf
-  depends_on :jasper
   depends_on :proj
-  depends_on :udunits
 
   def install
     ENV['LDFLAGS'] = "-L#{Eccodes.lib64}"
@@ -25,17 +23,13 @@ class Cdo < Package
       --with-netcdf=#{Netcdf.prefix}
       --with-zlib=#{Zlib.prefix}
       --with-szlib=#{Szip.prefix}
-      --with-jasper=#{Jasper.prefix}
       --with-eccodes=#{Eccodes.prefix}
-      --with-udunits2=#{Udunits.prefix}
       --with-proj=#{Proj.prefix}
-      --with-libxml2=#{Libxml2.prefix}
       --disable-dependency-tracking
       --disable-debug
       LIBS='-lz'
     ]
     run './configure', *args
-    # inreplace 'test/Makefile', 'CDO = $(top_builddir)/src/cdo' => 'CDO = $(top_builddir)/src/cdo -L'
     run 'make'
     run 'make', 'check' unless skip_test?
     run 'make', 'install'
